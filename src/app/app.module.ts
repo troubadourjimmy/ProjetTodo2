b5cf564a8d2b72b355efd358f18f2e81f4f0463f
troubadourjimmy <zhangxubeinicolas@gmail.com>
2022-02-16 15:22:41 +0100

    avant authentification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CreateTodoComponent } from './modals/create-todo/create-todo.component';
 import { ModifiTodoComponent } from './modals/modifi-todo/modifi-todo.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent,CreateTodoComponent,ModifiTodoComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,ReactiveFormsModule],
+  imports: [BrowserModule, 
+            IonicModule.forRoot(), 
+            AppRoutingModule,
+            ReactiveFormsModule,
+            // AngularFireModule.initializeApp(environment.firebase),
+            // AngularFireAnalyticsModule,
+            // AngularFirestoreModule
+          ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
