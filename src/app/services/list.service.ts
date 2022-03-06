ec00c5811942ee0e986fc68381ee1037419b127c
troubadourjimmy <zhangxubeinicolas@gmail.com>
2022-03-06 11:00:49 +0100

    afficher le todo detaille de firebase

--- src/app/services/list.service.ts
+++ src/app/services/list.service.ts
@@ -32,11 +32,12 @@ export class ListService {
     return Firestore.collectionData<List>(this.Lists$,{idField:'id'});
   }
 
-  //trouver le list choist
+  // //trouver le list choist
   getOne(id:string):List {
     return this.Lists.find(element=>element.id === id);
   }
-
+ 
+  //trouver le list choist
   getOneList(ListId: string):Observable<List>
   {
     //`todoLists/${ListId} est le chemain dans firebase
@@ -73,6 +74,18 @@ export class ListService {
   //trouver un todo chosit dans un list
   getTodo(listId:string, todoId:string):Todo {
      return this.getOne(listId).item.find(element=>element.id === todoId);
+  }
+
+  getOneTodo(listId:string, todoId:string):Observable<Todo> {
+    const doc = Firestore.doc(this.firestore, `todoLists/${listId}/todos/${todoId}`) as Firestore.DocumentReference<Todo>;
+    //const todosCollection$ = Firestore.collection(this.firestore,`todoLists/${listId}/todos`) as Firestore.CollectionReference<Todo>;
+    return Firestore.docData<Todo>(doc,{ idField: 'id'}).pipe(   
+        map(todo =>({
+          ...todo
+        }))
+      )
+    
+     
   }
 
 
