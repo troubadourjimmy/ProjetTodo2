f8a8070e394c94dda095a425650644b9fe18d1d4
troubadourjimmy <zhangxubeinicolas@gmail.com>
2022-03-30 14:41:15 +0200

    ajouter le angular guard pour redirect an unauthorized user to our login page but also redirect an authorized user who has not verified their email to a different page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
+import { AngularFireAuthGuard, redirectUnauthorizedTo,emailVerified, AuthPipeGenerator } from '@angular/fire/compat/auth-guard';
 import {AuthGuard} from '@angular/fire/auth-guard';
 import { reauthenticateWithRedirect } from '@angular/fire/auth';
+import { pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 
 const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+
+
+//If we need to redirect an unauthorized user to our login page but 
+//also redirect an authorized user who has not verified their email to a different page 
+const redirectUnauthorizedOrUnverifiedUser: AuthPipeGenerator = () =>
+  map(user => {
+    if (user) {
+      if (user.emailVerified) {
+        return true
+      } else {
+        return ['login']
+      }
+    } else {
+      return ['login']
+    }
+  })
+
 const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
     //les utilisateurs non authentifiés ne sont pas autorisés à naviguer vers homepage
     canActivate: [AngularFireAuthGuard],
-    data: { authGuardPipe: redirectUnauthorizedToLogin }
+    //data: { authGuardPipe: redirectUnauthorizedToLogin}
+    data: { authGuardPipe: redirectUnauthorizedOrUnverifiedUser }
   },
   {
     //pour aller au page login comme le premier page lorsque entrer le app
@@ -34,7 +54,9 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule),
+    
+    
   },
   {
     path: 'register',
